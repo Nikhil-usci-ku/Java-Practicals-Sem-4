095a13f73c5460acd75ed21f67ca9cbc32f807b0
Nikhil Kumar <20220702100@karnavatiuniversity.edu.in>
2024-03-06 16:23:26 +0530

    Created practical 20

---
+++ P20_Shopping_System.java
@@ -0,0 +1,85 @@
+class Product{
+    String productId;
+    String productName;
+    double price;
+    public Product(String Name,String ID,double Cost){
+    productId = ID;
+    productName = Name;
+    price= Cost;
+    };
+    public void Show_Product(){
+        System.out.println("Product Category : General");
+        System.out.println("Product ID : "+productId);
+        System.out.println("Product Name : "+productName);
+        System.out.println("Product Price : "+price+"\n");
+    };
+}
+
+class Electronic_P extends Product{
+    int wattage;
+    public Electronic_P(String Name,String ID,double Cost,int Watt){
+        super(Name, ID, Cost);
+        productId = ID;
+        productName = Name;
+        price = Cost;
+        wattage = Watt;
+    }
+    public void Show_Product(){
+        System.out.println("Product Category : Electronic");
+        System.out.println("Product ID : "+productId);
+        System.out.println("Product Name : "+productName);
+        System.out.println("Product Price : "+price);
+        System.out.println("Product Wattage : "+wattage+"W"+"\n");
+    };
+}
+
+class Clothing_P extends Product{
+    int size;
+    public Clothing_P(String Name,String ID,double Cost,int Cloth_Size){
+        super(Name, ID, Cost);
+        productId = ID;
+        productName = Name;
+        price = Cost;
+        size = Cloth_Size;
+    }
+    public void Show_Product(){
+        System.out.println("Product Category : Clothing");
+        System.out.println("Product Name : "+productId);
+        System.out.println("Product Name : "+productName);
+        System.out.println("Product Price : "+price);
+        System.out.println("Product Size : "+size+"\n");
+    };
+}
+class Books_P extends Product{
+    int page;
+    public Books_P(String Name,String ID,double Cost,int Total_Pages){
+        super(Name, ID, Cost);
+        productId = ID;
+        productName = Name;
+        price = Cost;
+        page = Total_Pages;
+    }
+    public void Show_Product(){
+        System.out.println("Product Category : Books");
+        System.out.println("Product Name : "+productId);
+        System.out.println("Product Name : "+productName);
+        System.out.println("Product Price : "+price);
+        System.out.println("Number of Pages : "+page);
+    };
+}
+
+public class P20_Shopping_System{
+    public static void main(String[] args) {
+        Product pro = new Product("Item","G001",1000.00);
+        pro.Show_Product();
+
+        Product Elect = new Electronic_P("laptop","E001",72359.51,350);
+        Elect.Show_Product();
+
+        Product Cloth = new Clothing_P("Shirt","C001",1750.25,40);
+        Cloth.Show_Product();
+
+        Product Book = new Books_P("Textbook","B001",560,778);
+        Book.Show_Product();
+    }
+}
